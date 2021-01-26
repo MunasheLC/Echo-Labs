6b4fcb3bcffd11f686cd628b706ca1d541ffbcbc
tuitef2 <fiona.tuite2@mail.dcu.ie>
2021-01-26 16:26:23 +0000

    uploading dashboard components

---
+++ src/Dashboard/src/components/Topnav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Topnav.css';
+
+function Topnav(){
+    return(
+        <>
+        <nav className="top_nav">
+            <div className="top_nav-container">
+                   <p>Echo</p><p1>Labs</p1>
+
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default Topnav;
